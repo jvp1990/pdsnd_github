749cd3b4b9fe92a9eb0e6fe6f1213ea2ec4cb0cf
JosephPoore <joe.poore90@gmail.com>
2018-12-16 16:17:20 -0800

    Docstring added detailing user options at the end of the program

--- bikeshare.py
+++ bikeshare.py
@@ -216,6 +216,7 @@ def main():
         user_stats(df)
         raw_data(df)
 
+        """Option for user to quit or restart the program."""
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
